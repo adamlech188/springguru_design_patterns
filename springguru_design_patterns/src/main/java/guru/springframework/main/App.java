66e270dd6a8b4f4a4cb90087268ab9151cb7d41e
rhitgal1 <rhitgal1@comporium.com>
2015-06-25 11:26:11 -0400

    initial commit

---
+++ springguru_design_patterns/src/main/java/guru/springframework/main/App.java
@@ -0,0 +1,13 @@
+package guru.springframework.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
